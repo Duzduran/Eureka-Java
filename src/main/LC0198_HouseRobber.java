334ac96503d9ce1a6a171336a3526bf2ba98a7f2
Duzduran <ahmetduzduran@gmail.com>
2022-12-02 04:11:32 +0100

    tsolution added for HouseRobber🚨

--- src/main/LC0198_HouseRobber.java
+++ src/main/LC0198_HouseRobber.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class LC0198_HouseRobber {
+import java.util.Arrays;
+
+public class LC0198_HouseRobber {
+    int[] arr;
+
+    public int rob(int[] nums) {
+        arr = new int[100];
+        Arrays.fill(this.arr,-1);
+
+        return robFrom(0,nums);
+
+    }
+
+    private int robFrom(int i, int[] nums){
+
+        if(i >= nums.length) return 0;
+
+        if(this.arr[i] > -1)
+            return this.arr[i];
+
+        int answer = Math.max(this.robFrom(i+1,nums),this.robFrom(i+2 , nums) + nums[i]);
+
+        this.arr[i] = answer;
+        return answer;
+    }
 }
