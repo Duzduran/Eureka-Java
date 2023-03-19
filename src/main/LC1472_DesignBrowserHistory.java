2e99a23810f7d37a72a8f6bbca0f9d5913f49b41
Duzduran <ahmetduzduran@gmail.com>
2023-03-19 19:48:04 +0100

    DesignBrowserHistory solutionn&test added🗳

--- src/main/LC1472_DesignBrowserHistory.java
+++ src/main/LC1472_DesignBrowserHistory.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class LC1472_DesignBrowserHistory {
+import java.util.Stack;
+
+public class LC1472_DesignBrowserHistory {
+
+    private Stack<String> history, future;
+    private String current;
+
+    public LC1472_DesignBrowserHistory(String homepage) {
+        history = new Stack<String>();
+        future = new Stack<String>();
+        current = homepage;
+    }
+
+    public void visit(String url) {
+        history.push(current);
+        current = url;
+        future = new Stack<String>();
+    }
+
+    public String back(int steps) {
+        while(steps > 0 && !history.empty()) {
+            future.push(current);
+            current = history.pop();
+            steps--;
+        }
+        return current;
+    }
+
+    public String forward(int steps) {
+        while(steps > 0 && !future.empty()) {
+            history.push(current);
+            current = future.pop();
+            steps--;
+        }
+        return current;
+    }
 }
--- src/test/LC1472_Test.java
+++ src/test/LC1472_Test.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class LC1472_Test {
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class LC1472_Test {
+
+    @Test
+    public void testVisit() {
+        LC1472_DesignBrowserHistory history = new LC1472_DesignBrowserHistory("google.com");
+        history.visit("yahoo.com");
+        history.visit("google.com");
+        assertEquals("yahoo.com", history.back(1));
+    }
+
+    @Test
+    public void testBack() {
+        LC1472_DesignBrowserHistory history = new LC1472_DesignBrowserHistory("google.com");
+        history.visit("yahoo.com");
+        history.visit("amazon.com");
+        assertEquals("yahoo.com", history.back(1));
+        assertEquals("google.com", history.back(1));
+    }
+
+    @Test
+    public void testForward() {
+        LC1472_DesignBrowserHistory history = new LC1472_DesignBrowserHistory("google.com");
+        history.visit("yahoo.com");
+        history.visit("amazon.com");
+        history.back(2);
+        assertEquals("yahoo.com", history.forward(1));
+    }
 }
